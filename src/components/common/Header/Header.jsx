5426ad7a1e132adf98eb79b87e0da4f7ca3b8ce5
21120036 <thienan2003bt@gmail.com>
2024-10-10 02:06:29 +0700

    3. Attempt to make the search text working as expected

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Flex, Image, Input } from "@chakra-ui/react";
+import { Avatar, Button, Flex, Image, Input, Text } from "@chakra-ui/react";
 import logo from '../../../assets/react.svg';
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -11,6 +11,7 @@ import { MdNotificationsActive } from "react-icons/md";
 import { RxDropdownMenu } from "react-icons/rx";
 
 import './Header.scss';
+import { RiDeleteBinLine } from "react-icons/ri";
 
 
 
@@ -20,17 +21,80 @@ function Header() {
 
 
     const [searchText, setSearchText] = useState("");
+    const [suggestedSearchResults, setSuggestedSearchResults] = useState([{text: 'Hello world!'}]);
+    const [isSearchMenuOpen, setIsSearchMenuOpen] = useState(false);
+    
     const [selectedNavigator, setSelectedNavigator] = useState("Home");
+
+    
+
+
+    const handleChangeTextSearch = (e) => {
+        setSearchText(e.target.value);
+        setIsSearchMenuOpen(true);
+    }
+
+    const handleSearch = async (e) => {
+        if (e.key !== "Enter" && e.key !== 'NumpadEnter') {
+            return;
+        }
+
+        window.alert("Has enter!");
+        // TODO: add calling API here
+        const data = [{
+            text: "Demo text",
+        }];
+        setSuggestedSearchResults(data);
+    }
+
+    const handleChooseSuggestedResult = async (id) => {
+        console.log("Has called  this");
+        window.alert("Has chosen the suggested result: " + id);
+    }
+
+    const handleDeleteSuggestedResult =  async (e, id) => {
+        e.preventDefault();
+        window.alert("Has deleted the suggested result: " + id);
+    }
+    
 
     return (
         <Flex w={"95vw"} className="app-header" justifyContent={"space-between"} gap={4} margin={"0 auto"}>
-            <Flex className="left-side-header" flex={30} w={"full"} justifyContent={"center"} gap={2}>
+            <Flex className="left-side-header" flex={30} w={"full"} justifyContent={"center"} gap={2}
+                position={"relative"}>
                 <Link to={"/"}>
                     <Image src={logo} alt="App logo" cursor={"pointer"} _hover={{opacity: 0.7}} />
                 </Link>
-                <Input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)}
-                    placeholder="Search here ..." w={"300px"} borderRadius={"2xl"}
-                />
+                <Flex position={"relative"} flexDirection={"column"} alignItems={"center"}>
+                    <Input type="text" value={searchText} onChange={(e) => handleChangeTextSearch(e)} onKeyDown={(e) => handleSearch(e)}
+                        placeholder="Search here ..." w={"300px"} borderRadius={"2xl"} 
+                        onFocus={() => setIsSearchMenuOpen(true)}
+                        onBlur={() => setIsSearchMenuOpen(false)}
+                        
+                    />
+
+                    <Flex position={"absolute"} top={"48px"} hidden={isSearchMenuOpen === false} 
+                    w={"full"} p={2} borderRadius={"0 0 12px 12px"} bg={"gray.900"}>
+                        {suggestedSearchResults.length > 0 
+                        && suggestedSearchResults.slice(0, 8).map((result, id) => {
+                            return (
+                                <Flex key={`search-result-${id}`} w={"full"} justifyContent={"space-between"} 
+                                    cursor={"pointer"} p={2} width={"full"}
+                                >
+                                    <Text flex={9} p={2} _hover={{backgroundColor: "rgba(255, 255, 255, 0.08)"}} 
+                                        onClick={() => handleChooseSuggestedResult(id)}
+                                        textAlign={"start"}>
+                                        {result?.text}
+                                    </Text>
+                                    <RiDeleteBinLine flex={1} size={40} style={{padding: '8px'}} 
+                                        _hover={{backgroundColor: "rgba(255, 255, 255, 0.08)"}} 
+                                        onClick={(e) => handleDeleteSuggestedResult(e, id)}
+                                    />
+                                </Flex>
+                            )
+                        })}
+                    </Flex>
+                </Flex>
             </Flex>
 
             <Flex className="center-side-header" flex={40} justifyContent={"space-evenly"}>
@@ -63,9 +127,6 @@ function Header() {
                     <MdNotificationsActive size={ICON_SIZE} />
                 </Button>
                 
-                {/* <Button className="header-icon" >
-                    <MdNotificationsActive size={ICON_SIZE} />
-                </Button> */}
                 <Avatar src="" w={"38px"} h={"38px"} _hover={{
                     opacity: 0.8,
                     cursor: "pointer",
