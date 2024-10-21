216c23b47edcc83d6ebfdafaedc3a53601d51727
21120036 <thienan2003bt@gmail.com>
2024-10-21 23:44:47 +0700

    30. Basic fix for loading frontend in docker. Still have bug when routing /auth and /count

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,33 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // port: 3000,
+    host: '0.0.0.0',  // Ensure Docker can expose the server
+    proxy: {
+      // Proxy API requests to the backend during development
+      '/api': {
+        target: 'http://localhost:8100',  // Change to backend URL in development
+        changeOrigin: true,
+      }
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      }
+    }
+  },
+  //  Add this:
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  // Fix for SPA routing issues
+  historyApiFallback: true,
 })
